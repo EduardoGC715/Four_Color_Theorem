257c4178741d30692c89c1fb1564b2d25b63f226
EduardoGC715 <99993140+EduardoGC715@users.noreply.github.com>
2022-08-13 21:37:21 -0600

    Terminado

--- main.cpp
+++ main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//Constants
+#define MAPA 4
+
+//Functions
+void four_colors(short pMatriz[MAPA][MAPA]){
+    bool posible=true;
+    short colores[MAPA];
+    colores[0]=1;
+    for (int x=0;x<MAPA;x++){
+        int col = 1;
+        for (int y=0;y<MAPA;y++){
+            if (pMatriz[x][y]==1) {
+                for (; col <5;col++){
+                    colores[x]=col;
+                    if (colores[y] != colores[x]) {
+                        break;
+                    }
+                    else if(colores[y]==colores[x] && col==4){
+                        posible=false;
+                    }
+                    else if (x+1==MAPA){
+                        y=0;
+                    }
+                }
+            }
+        }
+    }
+    if(posible){
+        cout<<"Si es posible aplicar el teorema.\n";
+        for (int i=0; i<MAPA; i++){
+            cout<<i+1<<" es de color "<<colores[i]<<"\n";
+        }
+    }
+    else{
+        cout<<"No es posible aplicar el teorema."<<endl;
+    }
+}
+
+void matriz_automatica(){
+    short matrizr[MAPA][MAPA];
+    srand (time(NULL));
+    for (int x=0;x<MAPA;x++) {
+        for (int y = 0; y < MAPA; y++) {
+            if (x>=y+1){
+                matrizr[x][y]=rand()%2;
+            }
+            else{
+                matrizr[x][y]=0;
+            }
+        }
+    }
+    for (int x=0;x<MAPA;x++) {
+        for (int y = 0; y < MAPA; y++) {
+            cout<<matrizr[x][y]<<" ";
+        }
+        cout<<endl;
+    }
+    four_colors(matrizr);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+   /* short matrizr[MAPA][MAPA]={
+            {0,0,0,0},
+            {1,0,0,0},
+            {1,1,0,0},
+            {0,1,1,0}
+    };
+    four_colors(matrizr);*/
+    matriz_automatica();
     return 0;
 }
